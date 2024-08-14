d03fbcd11f3d787f754c909fbe0b1c616bdad8af
Joe Nevin <josephfnevin@gmail.com>
2024-08-14 15:09:36 -0500

    Starting React Next setup.

---
+++ components/Chat.tsx
@@ -0,0 +1,9 @@
+const Chat = () => {
+  return (
+    <div>
+      <h1>Chat Sir</h1>
+    </div>
+  );
+};
+
+export default Chat;
---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Chat from '../../components/Chat';
+
+const Home = () => {
+  return (
+    <main>
+      <Chat />
+    </main>
+  );
+};
+
+export default Home;
